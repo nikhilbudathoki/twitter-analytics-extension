9e87cd1456d856a55d1d67be68595e802d4846ab
Nikhil Budathoki <niksbuds27@gmail.com>
2023-05-30 16:52:21 -0700

    added toggle functionality

--- extension/side-panel.js
+++ extension/side-panel.js
@@ -1,3 +1,12 @@
+console.log("side-panel script loaded");
+
+chrome.runtime.onMessage.addListener(function(msg, sender){
+    if(msg == "toggle"){
+        console.log("message received");
+        toggle();
+    }
+})
+
 var iframe = document.createElement('iframe'); 
 iframe.style.background = "green";
 iframe.style.height = "100%";
@@ -10,3 +19,12 @@ iframe.style.border = "0px";
 iframe.src = chrome.runtime.getURL("popup.html")
 
 document.body.appendChild(iframe);
+
+function toggle(){
+    if(iframe.style.width == "0px"){
+        iframe.style.width="400px";
+    }
+    else{
+        iframe.style.width="0px";
+    }
+}
